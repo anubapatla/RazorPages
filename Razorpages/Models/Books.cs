ade10357c13a9b133ca464c13cb73d72421a0f9f
AnuBapatla <anusrichatala@gmail.com>
2026-04-09 14:03:52 -0400

    book class hasbeen created in model

--- Razorpages/Models/Books.cs
+++ Razorpages/Models/Books.cs
@@ -1,6 +1,17 @@
-﻿namespace Razorpages.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Razorpages.Models
 {
     public class Books
     {
+        public int Id { get; set; }
+        [Required]
+        [DisplayColumn("Book Title")]
+        public string BookTitle { get; set; }
+
+        [DisplayColumn("BookDescription")]
+        public string BookDescription { get; set; }
+        [Required]
+        public string Author { get; set; }
     }
 }
